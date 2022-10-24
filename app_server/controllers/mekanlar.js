504c5e6fbdb6fdcc4104b7bf8ee2f300243b7874
AliTokbas <alitokbas2000@hotmail.com>
2022-10-24 17:19:05 +0300

    24 ekim

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -4,12 +4,83 @@ var router = express.Router();
 
 
 const anaSayfa=function(req, res, next) {
-    res.render('anasayfa', { title: 'Anasayfa' });
+    res.render('anasayfa', 
+    { 
+    "baslik":"Anasayfa",
+    "sayfaBaslik": {
+    "siteAd":"MekanBul",
+    "slogan":"Civardaki Mekanları Keşfet!"
+    },
+    "mekanlar":[
+        {
+            "ad":"Starbucks",
+            "puan":"3",
+            "adres":"Centrum Garden AVM",
+            "imkanlar":["Kahve","Çay","Tatlı Çeşitleri"],
+            "mesafe":"10km"
+        },
+        {
+            "ad":"Barida Kafe",
+            "puan":"4",
+            "adres":"SDU Batı Kampüsü",
+            "imkanlar":["Kahve","Çay","Tatlı Çeşitleri"],
+            "mesafe":"1km"
+        },
+
+    ]
+});
 };
 
 
 const mekanBilgisi=function(req, res, next) {
-    res.render('mekanbilgisi', { title: 'Mekan Bilgisi' });
+    res.render('mekanbilgisi', 
+    {
+        "baslik":"Mekan Bilgisi",
+        "mekanBaslik":"Starbucks",
+        "mekanDetay":{
+            "ad":"Starbucks",
+            "puan":"3",
+            "adres":"Centrum Garden AVM",
+            "imkanlar":["Kahve","Çay","Tatlı Çeşitleri"],
+            "koordinatlar":{
+                "enlem":"37.7",
+                "boylam":"30.5"
+            },
+            "saatler":[
+                {
+                "gunler":"Pazartesi-Cuma",
+                "acilis":"9:00",
+                "kapanis":"23:00",
+                "kapali": false
+                },
+                {
+                "gunler":"Cumartesi-Pazar",
+                "acilis":"8:00",
+                "kapanis":"22:00",
+                "kapali": false
+                }
+
+            ],
+          
+            "yorumlar":[
+                {
+                    "yorumYapan":"Ali Tokbaş",
+                    "yorumMetni":"Eh",
+                    "yorumTarihi":"20 Ekim 2022",
+                    "puan":"2"
+                },
+                {
+                    "yorumYapan":"Ali Tokbaş",
+                    "yorumMetni":"Eh",
+                    "yorumTarihi":"20 Ekim 2022",
+                    "puan":"2"
+                }
+            ]
+           
+
+
+        }
+     });
 };
 
 const yorumEkle=function(req, res, next) {
