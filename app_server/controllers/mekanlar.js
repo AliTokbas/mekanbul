4e029b98d36aa97a81eb8ba2c7f31933aacf95b7
AliTokbas <alitokbas2000@hotmail.com>
2022-12-09 17:23:11 +0300

    9Aralık

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,6 +1,6 @@
 const axios=require('axios');
 var apiSecenekleri ={
-    sunucu: "https://mekanbul.alitokbas.repl.co",
+    sunucu: "http://localhost:3000",
     apiYolu:"/api/mekanlar/",
 };
 var express = require('express');
@@ -89,6 +89,7 @@ const mekanBilgisi=function(req, res) {
         .get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid)
         .then(function (response)
         {
+            req.session.mekanAdi=response.data.ad
             detaySayfasiOlustur(res, response.data);
         })
         .catch(function (hata)
@@ -99,9 +100,30 @@ const mekanBilgisi=function(req, res) {
 };
 
 const yorumEkle=function(req, res, next) {
-    res.render('yorumekle', { title: 'Yorum Ekle' });
+    var mekanAdi=req.session.mekanAdi;
+    var mekanid=req.params.mekanid;
+    if(!mekanAdi){
+        res.redirect("/mekan/"+mekanid);//başka yere yönlendirme
+    } else
+    res.render('yorumekle', { "baslik":mekanAdi+" mekanına yorum ekle",title: 'Yorum Ekle' });
 };
 
+const yorumumuEkle=function(req, res, next) {
+    var gonderilenYorum,mekanid;
+    mekanid=req.params.mekanid;
+    if(!req.body.adsoyad || !req.body.yorum){
+        res.redirect("/mekan/"+mekanid+"/yorum/yeni?hata=evet");
+    } else{
+        gonderilenYorum={
+            yorumYapan:req.body.adsoyad,
+            puan:req.body.puan,
+            yorumMetni:req.body.yorum
+        }
+        axios.post(apiSecenekleri.sunucu+apiSecenekleri.apiYolu+mekanid+"/yorumlar",gonderilenYorum).then(function(){
+            res.redirect("/mekan/"+mekanid);
+        });
+    }
+};
 
 
 
@@ -109,5 +131,6 @@ module.exports={
     anaSayfa,
     mekanBilgisi,
     yorumEkle,
+    yorumumuEkle
 
 }
